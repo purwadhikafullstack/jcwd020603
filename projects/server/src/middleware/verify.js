e037cde9bfdcbe33de2eae836ec1e6ef29acfbf8
MUHAMMAD MUBAARAK <126537753+baarak08@users.noreply.github.com>
2023-08-28 18:01:51 +0700

    update secret_key

--- projects/server/src/middleware/verify.js
+++ projects/server/src/middleware/verify.js
@@ -1,4 +1,5 @@
 const key = process.env.secret_key;
+console.log("ini key", process.env.secret_key);
 const verify = async (req, res, next) => {
   const secret = req.headers["x-secret-key"];
   if (secret != key) {
